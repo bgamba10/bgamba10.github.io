2e5f725b7b0669654b656557b6fd331f7497d1ee
Actualización 2:59pm <bibianagambas@Bibianas-MacBook-Pro.local>
2018-02-11 22:16:07 -0500

    new images

---
+++ js/script.js
@@ -0,0 +1,7 @@
+   function showDiv(skills)
+   {
+       if(document.getElementById(divID).style.display=='none')
+       {
+           document.getElementById(divID).style.display='block';  
+       }
+}
