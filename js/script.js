ec8f1eb19190eed2268db657d0d95a7d6cd7fded
Actualización 2:59pm <bibianagambas@Bibianas-MacBook-Pro.local>
2018-02-13 18:31:37 -0500

    new images new js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-   function showDiv(skills)
-   {
-       if(document.getElementById(divID).style.display=='none')
-       {
-           document.getElementById(divID).style.display='block';  
-       }
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 800);
+    return false;
+});
+
+$('.resizableImage').hover(makeBigger, returnToOriginalSize);
+
+function makeBigger() {
+    $(this).css({height: '+=10%', width: '+=10%'});
+}
+function returnToOriginalSize() {
+    $(this).css({height: "", width: ""});
 }
