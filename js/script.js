10e9a7edbe0eb2cbb309ffa60de57cca5ee36734
Actualización 2:59pm <bibianagambas@Bibianas-MacBook-Pro.local>
2018-02-14 11:55:12 -0500

    Js with linter

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
+/* global $ */
 "use-strict";
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
+$("a").click(function(){
+    $("html, body").animate({
+        scrollTop: $( $(this).attr("href") ).offset().top
     }, 900);
     return false;
 });
 
-$('.makeImageBigger').hover(bigger, backToOriginal);
+$(".makeImageBigger").hover(bigger, backToOriginal);
 
 function bigger() {
-    $(this).css({height: '+=10%', width: '+=10%'});
+    $(this).css({height: "+=10%", width: "+=10%"});
 }
 function backToOriginal() {
     $(this).css({height: "", width: ""});
